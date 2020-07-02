b474343ce4df6e9f8c9673148d0016a491846ad8
Robson Escotiel <robsonesr@gmail.com>
2020-07-02 13:33:02 -0300

    Listando todas as categorias com DTO

---
+++ src/main/java/com/escotiel/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,39 @@
+package com.escotiel.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.escotiel.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String nome;
+
+	public CategoriaDTO() {}
+	
+	public CategoriaDTO(Categoria obj) {
+		Id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
+
--- src/main/java/com/escotiel/cursomc/services/CategoriaService.java
+++ src/main/java/com/escotiel/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.escotiel.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,4 +51,9 @@ public class CategoriaService {
 		}
 
 }
+
+	public List<Categoria> findAll() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
 }
