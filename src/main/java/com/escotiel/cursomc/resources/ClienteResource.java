77b1e0a9ffc2e96d5be7ec7d86d4799e6751b6fc
Robson Escotiel <robsonesr@gmail.com>
2020-07-02 23:31:08 -0300

    Put, Delete e Get para Cliente

--- src/main/java/com/escotiel/cursomc/resources/ClienteResource.java
+++ src/main/java/com/escotiel/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.escotiel.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.escotiel.cursomc.domain.Cliente;
+import com.escotiel.cursomc.dto.ClienteDTO;
 import com.escotiel.cursomc.services.ClienteService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -25,17 +34,59 @@ public class ClienteResource {
 		
 		Cliente obj = service.find(id);
 		
-		//Categoria cat1 = new Categoria(1,"Informática");
-		//Categoria cat2 = new Categoria(2,"Escritório");
+		//Cliente cat1 = new Cliente(1,"Informática");
+		//Cliente cat2 = new Cliente(2,"Escritório");
 		
 		return ResponseEntity.ok(obj);
 		
 		/*lista de categorias
-		List<Categoria> lista = new ArrayList<>();
+		List<Cliente> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
 		
 		return lista; //retorna a lista no formato JSON*/
 	}
+	
+	//método de atualização
+		@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+		public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) throws ObjectNotFoundException{
+			Cliente obj = service.fromDTO(objDto);
+			obj.setId(id);
+			obj = service.update(obj);
+			return ResponseEntity.noContent().build();
+		}
+	 
+		
+		//método de deleção
+		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Integer id) throws ObjectNotFoundException {
+		
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+		
+		//listar todas categorias
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<ClienteDTO>> findAll() {
+			
+			List<Cliente> list = service.findAll();
+			List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+			
+			return ResponseEntity.ok().body(listDto);
+		}
+		
+		//listar categorias paginadas
+		@RequestMapping(value="/page", method=RequestMethod.GET)
+		public ResponseEntity<Page<ClienteDTO>> findPage(
+				@RequestParam(value="page", defaultValue = "0") Integer page, 
+				@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage, //usa o 24
+				@RequestParam(value="orderBy", defaultValue = "nome")String direction, 
+				@RequestParam(value="direction", defaultValue = "ASC")String orderBy) {
+			
+			Page<Cliente> list = service.findPage(page, linesPerPage, direction, orderBy);
+			Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+			
+			return ResponseEntity.ok().body(listDto);
+		}
 
 }
--- src/main/java/com/escotiel/cursomc/services/ClienteService.java
+++ src/main/java/com/escotiel/cursomc/services/ClienteService.java
@@ -1,27 +1,77 @@
 package com.escotiel.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.escotiel.cursomc.domain.Cliente;
+import com.escotiel.cursomc.dto.ClienteDTO;
 import com.escotiel.cursomc.repositories.ClienteRepository;
+import com.escotiel.cursomc.services.exceptions.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
-	//chama repository
-	@Autowired //dependencia automaticamente injetada pelo spring
+
+	// chama repository
+	@Autowired // dependencia automaticamente injetada pelo spring
 	private ClienteRepository repo;
-	
-	//operação buscar cliente por código
+
+	// operação buscar cliente por código
 	public Cliente find(Integer id) throws ObjectNotFoundException {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) throws ObjectNotFoundException {
+
+		// pega o cliente do BD
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);// como o id não é nulo (existe no bd) ele atualiza
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) throws ObjectNotFoundException {
+		// verificar se o objeto existe usando o método find
+		find(id);
+		// cria uma exceção personalizada para caso se tente apagar algum objeto que
+		// esteja relacionado com outro (integridade referencial)
+		// cria no pacote exceptions
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+		}
+
+	}
+
+	// listar todas as categorias
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	// listar categorias com paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 
 }
